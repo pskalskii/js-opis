84fce9887497d006f53374bcd1cb9c585b22d319
pskalskii <pawelskalski97@gmail.com>
2022-05-04 12:11:43 +0200

    wyświetlanie opisu

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let paragraph = "p";
+document.getElementById("opis");
+
+let firstButton = document.querySelector("button");
+document.querySelector("button").onclick = function () {
+  opis.textContent = "Losowy Napis";
+};
